6c65b2144ca58815b2b2e09dc8730b3bb0138b10
hung.nguyen <linqtojs@gmail.com>
2022-08-16 20:22:20 +0700

    fix: navigation state

--- feats/navigation/sagas/navigationBootstrap.ts
+++ feats/navigation/sagas/navigationBootstrap.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SC } from 'rativ/saga';
 
-import { Linking, Platform } from 'react-native';
+import { Linking } from 'react-native';
 
 import { navigationStateAtom } from '../atoms/navigationStateAtom';
 import { navigationSideEffects } from './navigationSideEffects';
@@ -11,10 +11,11 @@ import { PERSISTENCE_KEY } from '@/configs/navigation';
 const navigationBoostrap = async ({ spawn, set }: SC) => {
   const initialUrl = await Linking.getInitialURL();
 
-  if (Platform.OS !== 'web' && initialUrl) {
+  if (initialUrl) {
     // Only restore state if there's no deep link and we're not on web
     const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
     const state = savedStateString ? JSON.parse(savedStateString) : undefined;
+
     if (state) {
       await set(navigationStateAtom, state);
     }
