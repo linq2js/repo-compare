5806539503fe2b1f915629fc7b781fc41b49c57f
hung.nguyen <linqtojs@gmail.com>
2022-08-16 13:46:01 +0700

    docs refactor: login form

--- feats/form/comps/TextField.tsx
+++ feats/form/comps/TextField.tsx
@@ -3,15 +3,18 @@ import { Input } from 'native-base';
 import { FieldPath, PathValue, useController } from 'react-hook-form';
 
 import { FieldProps } from '../types';
+import { FieldError, Props as ErrorProps } from './FieldError';
 
 import { PropsOf } from '@/types';
 
-export type Props<T, N extends FieldPath<T>> = FieldProps<T, N> & PropsOf<typeof Input>;
+export type Props<T, N extends FieldPath<T>> = FieldProps<T, N> &
+  PropsOf<typeof Input> & { errorProps?: Omit<ErrorProps<T, N>, 'control' | 'name'> };
 
 const TextField = <T, N extends FieldPath<T>>({
   name,
   control,
   defaultValue,
+  errorProps,
   rules,
   ...props
 }: Props<T, N>) => {
@@ -22,13 +25,26 @@ const TextField = <T, N extends FieldPath<T>>({
     rules,
   });
 
-  return (
+  const inputNode = (
     <Input
       {...props}
       value={field.value as string}
       onChangeText={field.onChange}
       onBlur={field.onBlur}
     />
+  );
+
+  const errorNode = errorProps && <FieldError control={control} name={name} {...errorProps} />;
+
+  if (!errorNode) {
+    return inputNode;
+  }
+
+  return (
+    <>
+      {inputNode}
+      {errorNode}
+    </>
   );
 };
 
--- feats/user/screens/LoginScreen.tsx
+++ feats/user/screens/LoginScreen.tsx
@@ -1,14 +1,12 @@
-import { Button } from 'native-base';
+import { Button, Heading, VStack } from 'native-base';
 import { stable } from 'rativ/dist/tsc/react';
 
 import { useForm } from 'react-hook-form';
 
 import { loginSignal } from '../signals/loginSignal';
 
-import { FieldError } from '@/form/comps/FieldError';
 import { TextField } from '@/form/comps/TextField';
 import { ThemedBox } from '@/shared/comps/ThemedBox';
-import { ThemedText } from '@/shared/comps/ThemedText';
 import { createScreen } from '@/shared/helpers/createScreen';
 
 export type LoginFormModel = {
@@ -28,23 +26,26 @@ const LoginScreenImpl = stable(() => {
     const { control, handleSubmit: onSubmit } = useForm<LoginFormModel>();
 
     return (
-      <ThemedBox flex={1}>
-        <ThemedText>Login Screen</ThemedText>
-        <TextField
-          control={control}
-          name="username"
-          rules={{ required: true }}
-          placeholder="Enter any username to login"
-        />
-        <FieldError control={control} name="username" message="Invalid email or password" />
-        <TextField
-          control={control}
-          name="password"
-          rules={{ required: true }}
-          placeholder="123456"
-        />
-        <FieldError control={control} name="password" message="Invalid email or password" />
-        <Button onPress={onSubmit(handleSubmit)}>Submit</Button>
+      <ThemedBox flex={1} p={4}>
+        <VStack space="md">
+          <Heading>Login</Heading>
+          <TextField
+            control={control}
+            name="username"
+            rules={{ required: true }}
+            placeholder="Enter any username to login"
+            errorProps={{ message: 'Invalid email or password' }}
+          />
+
+          <TextField
+            control={control}
+            name="password"
+            rules={{ required: true }}
+            placeholder="123456"
+            errorProps={{ message: 'Invalid email or password' }}
+          />
+          <Button onPress={onSubmit(handleSubmit)}>Submit</Button>
+        </VStack>
       </ThemedBox>
     );
   };
